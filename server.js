6b983d8cc0d2b2c51341d0be71882cea0dbfee16
pradeep <pradeepbawari@gmail.com>
2025-12-19 14:49:35 +0530

    add cross origin

--- server.js
+++ server.js
@@ -43,14 +43,17 @@ app.options('*', cors(corsOptions)); // Handle preflight requests
 
 
 // Set the port production
-//  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 // Set the port local
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/webhooks', webhookRoutes);
+
+app.use(bodyParser.json());
+app.use(express.json());
 
 // Initialize the multer upload middleware
 const createUpload = upload.array('images', 10); 
@@ -92,6 +95,12 @@ db.sequelize
 //   .then(() => console.log('Database connected bawari...'))
 //   .catch((err) => console.log('Error: '));
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+module.exports = app;
+
+// Local run only
+if (require.main === module) {
+  //const PORT = process.env.PORT || 4000;
+  //const PORT = process.env.PORT || 7000;
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+}
+
