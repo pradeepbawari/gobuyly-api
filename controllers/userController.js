0ed40fcdadb7af1f3614d789adef5442fc7835e8
pradeep <pradeepbawari@gmail.com>
2026-01-07 13:48:00 +0530

    user shipping update

--- controllers/userController.js
+++ controllers/userController.js
@@ -108,11 +108,26 @@ const getUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
 // Update User
 const updateUser = async (req, res) => {
   try {
-    const { id, ...updateFields } = req.body;
+    console.log(id, ...updateFields)
+    if (!id) return res.status(400).json({ message: 'User ID is required' });
+
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    await user.update(updateFields);
+    res.status(200).json({ message: 'User updated successfully', user });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+// Update User
+const updateUserfron = async (req, res) => {
+  try {
+    const { id, ...updateFields } = req?.body;
     if (!id) return res.status(400).json({ message: 'User ID is required' });
 
     const user = await User.findByPk(id);
@@ -155,4 +170,4 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser, getUsers, getUser, updateUser, deleteUser, loginUser };
+module.exports = { createUser, getUsers, getUser, updateUser, deleteUser, loginUser, updateUserfron };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 // routes/userRoutes.js
 const express = require('express');
-const { createUser, getUsers, getUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
+const { createUser, getUsers, getUser, updateUser, deleteUser, loginUser, updateUserfron } = require('../controllers/userController');
 const router = express.Router();
 
 // POST: Create a new user
@@ -16,6 +16,7 @@ router.get('/users/:id', getUser);
 
 // PUT: Update a user by ID
 router.put('/update/:id', updateUser);
+router.put('/userupdate', updateUserfron);
 
 // DELETE: Delete a user by ID
 router.delete('/delete/:id', deleteUser);
